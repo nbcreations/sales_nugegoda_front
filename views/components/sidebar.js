b0b9eeff1c5a1ea06e38dd4616c64000445e8c2f
nbcreations <156303656+nbcreations@users.noreply.github.com>
2024-03-21 22:59:39 +0530

    Update sidebar.js

--- views/components/sidebar.js
+++ views/components/sidebar.js
@@ -5,27 +5,14 @@ const sidebar =
         <div class="sidenav-menu">
             <div class="nav accordion" id="accordionSidenav">
                 
-                <div class="sidenav-menu-heading">Product Management</div>
+                <div class="sidenav-menu-heading">Order Management</div>
                 
-                <a class="nav-link" href="/product">
-                    <div class="nav-link-icon"><i class="fa-solid fa-gift"></i></div>Product List
+                <a class="nav-link" href="/orders">
+                    <div class="nav-link-icon"><i class="fa-solid fa-gift"></i></div>Order List
                 </a>
 
-                <a class="nav-link" href="/product-add">
-                    <div class="nav-link-icon"><i class="fa-solid fa-layer-group"></i></div>Product Add
-                </a>
-
-
-                <!-- Sidenav Menu Heading (Core)-->
-                <div class="sidenav-menu-heading">Sub Category Management</div>
-                <!-- Sidenav Accordion (Dashboard)-->
-                
-                <a class="nav-link" href="/sub-category-list">
-                    <div class="nav-link-icon"><i class="fa-solid fa-gift"></i></div>Sub Category List
-                </a>
-
-                <a class="nav-link" href="/sub-category-add">
-                    <div class="nav-link-icon"><i class="fa-solid fa-gift"></i></div>Sub Category Add
+                <a class="nav-link" href="/order-payment">
+                    <div class="nav-link-icon"><i class="fa-solid fa-layer-group"></i></div>Order Payment
                 </a>
 
             </div>
